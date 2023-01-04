248217f5d448c60535f170ce7c98bd1f898ee971
Marcelo <marcelo.leite@mercadolivre.com>
2023-01-04 18:04:11 -0300

    [feat] config H2 application.yml

--- src/main/kotlin/com/example/car/interfaces/DriverAPI.kt
+++ src/main/kotlin/com/example/car/interfaces/DriverAPI.kt
@@ -1,6 +1,7 @@
 package com.example.car.interfaces
 
 import com.example.car.domain.entity.Driver
+import com.example.car.domain.repository.DriverRepository
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController
 @Service
 @RestController
 @RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
-class DriverAPI {
+class DriverAPI(
+    val driverApiRepository: DriverRepository
+) {
 
     @GetMapping("/api/v1/drivers")
-    fun listDrivers(): List<Driver> = emptyList()
+    fun listDrivers(): List<Driver> {
+        return driverApiRepository.findAll()
+    }
 }
