08eab74697e90921b67d39b6ca1160e4887ce78f
Marcelo <marcelo.leite@mercadolivre.com>
2023-01-06 11:24:20 -0300

    [feat] throw an exception when getDriverById not found

--- src/main/kotlin/com/example/car/domain/useCase/DriverAPIService.kt
+++ src/main/kotlin/com/example/car/domain/useCase/DriverAPIService.kt
@@ -2,7 +2,9 @@ package com.example.car.domain.useCase
 
 import com.example.car.domain.entity.Driver
 import com.example.car.domain.repository.DriverRepository
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 
 @Service
 class DriverAPIService(
@@ -14,6 +16,8 @@ class DriverAPIService(
     }
 
     fun getDriverById(driverId: Long): Driver {
-        return driverRepository.getReferenceById(driverId)
+        return driverRepository.findById(driverId).orElseThrow {
+            ResponseStatusException(HttpStatus.NOT_FOUND)
+        }
     }
 }
